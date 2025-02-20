3e5e8c78d0f39138c91ed48a37ef95c28e155d91
ko1N <ko1N1337@gmail.com>
2025-02-20 22:13:26 +0100

    Add flash.nvim to improve search

--- dotfiles/nvim/.config/nvim/lua/plugins/rust.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/rust.lua
@@ -6,12 +6,16 @@ return {
       require("crates").setup()
     end,
   },
-  {
-    "cordx56/rustowl",
-    dependencies = { "neovim/nvim-lspconfig" },
-    config = function()
-      local lspconfig = require "lspconfig"
-      lspconfig.rustowlsp.setup {}
-    end,
-  },
+  -- {
+  --   "cordx56/rustowl",
+  --   dependencies = { "neovim/nvim-lspconfig" },
+  --   config = function()
+  --     local lspconfig = require "lspconfig"
+  --     lspconfig.rustowlsp.setup {
+  --       trigger = {
+  --         hover = true,
+  --       },
+  --     }
+  --   end,
+  -- },
 }
